bc6707d2227c1ffb0b86497ccec18526245f8fca
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-05-20 11:09:58 -0300

    Calculadora suma, resta, mult, div

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("valor de b:"+b);
 */
 //punto1
 
-int number, invertednumb = 0,digit,large;
+/*int number, invertednumb = 0,digit;
 bool parseBool;
 string? input;
 Console.WriteLine("Ingrese un numero:\n");
@@ -28,6 +28,56 @@ while(number > 0 )
 }
 
   Console.WriteLine($"numero inv {invertednumb}");
+*/
+
+string? input, siNoStr;
+bool parseoBool;
+int siNo;
+float a, b,division;
+
+
+do{
+    Console.WriteLine("Ingrese la operacion: ");
+    Console.WriteLine("1 - SUMAR");
+    Console.WriteLine("2 - RESTAR");
+    Console.WriteLine("3 - MULTIPLICAR");
+    Console.WriteLine("4 - DIVIDIR");
+    
+    input = Console.ReadLine();
+    parseoBool = int.TryParse(input, out int operacion);
+    if(operacion>=1 && operacion<6){
+    Console.WriteLine("Ingrese dos numeros: ");
+    parseoBool = float.TryParse(Console.ReadLine(), out  a);
+    parseoBool = float.TryParse(Console.ReadLine(), out  b);
+
+    switch(operacion){
+        case 1:
+            Console.WriteLine($"La suma del numero {a} y {b} es: {a + b}");
+            break;
+        case 2:
+            Console.WriteLine($"La resta del numero {a} y {b} es: {a - b}");
+            break;
+        case 3:
+            Console.WriteLine($"La multiplicación del numero {a} y {b} es: {a * b}");
+            break;
+        case 4:
+            division =  a / b;
+            Console.WriteLine($"La división del numero {a} y {b} es: {division.ToString("N2")}");
+            break;
+        default:
+            Console.WriteLine("El numero ingresado no corresponde a ninguna operación");
+            break;
+    }
+}
+
+    Console.WriteLine("Quiere seguir operando: SI[1] - NO[0]");
+    siNoStr = Console.ReadLine();
+    parseoBool = int.TryParse(siNoStr, out siNo);
+    
+}while(siNo == 1);
+
+Console.WriteLine("Finalizado");
+
 
 
 
